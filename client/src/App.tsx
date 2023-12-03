7ea2fdf4cd529385aaeddb65f838123bab548648
SylvainAlx <sylval49@gmail.com>
2023-12-03 21:02:06 +0100

    client - test router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,21 +3,20 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { publicRoutes } from "./router/routes";
 import { ArchipelRoute } from "./utils/types";
+import { BrowserRouter } from "react-router-dom";
 import Header from "./layouts/header";
 import Footer from "./layouts/footer";
 
 export default function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <main className="">
-        <Routes>
-          {publicRoutes.map((route: ArchipelRoute, i: number) => (
-            <Route path={route.path} element={route.page} key={i} />
-          ))}
-        </Routes>
-      </main>
+      <Routes>
+        {publicRoutes.map((route: ArchipelRoute, i: number) => (
+          <Route path={route.path} element={route.page} key={i} />
+        ))}
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
