49491ac479736df460c0f902bfd9e099f367bf52
ATELOG2I\Alexandre <sylvain.alexandre@atelog2i.fr>
2023-12-06 11:33:37 +0100

    modif router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,22 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { publicRoutes } from "./router/routes";
-import { ArchipelRoute } from "./utils/types";
-import { BrowserRouter } from "react-router-dom";
 import Header from "./layouts/header";
 import Footer from "./layouts/footer";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <>
       <Header />
       <Routes>
-        {publicRoutes.map((route: ArchipelRoute, i: number) => (
-          <Route path={route.path} element={route.page} key={i} />
-        ))}
+        <Route path="/" element= {<Home/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </>
   );
 }
