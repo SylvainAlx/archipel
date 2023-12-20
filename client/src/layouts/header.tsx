c90569a79189b69c5acd2b140a26bb042ff8be7c
ATELOG2I\Alexandre <sylvain.alexandre@atelog2i.fr>
2023-12-20 12:08:09 +0100

    client - responsive correction

--- client/src/layouts/header.tsx
+++ client/src/layouts/header.tsx
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router-dom";
 import NavBar from "../components/navbar";
-import { SUBTITLE, TITLE } from "../utils/consts";
 
 export default function Header() {
   const navigate = useNavigate();
