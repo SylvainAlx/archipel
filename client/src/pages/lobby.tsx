b5b72db86c870bbe3b686bed6f8e3765e8ba0d5a
ATELOG2I\Alexandre <sylvain.alexandre@atelog2i.fr>
2024-01-16 16:55:51 +0100

    client - gestion coms correction

--- client/src/pages/lobby.tsx
+++ client/src/pages/lobby.tsx
@@ -29,7 +29,7 @@ export default function Lobby(){
             <H1 text={TITLE} />
             <p className="max-w-[80%] text-center">Renseignez le mot de passe pour accéder à l'application</p>
             <form className="flex flex-col items-center justify-center gap-2" onSubmit={handleSubmit}>
-                <Input type="password" placeholder="Mot de passe" required value={password} onChange={handleChange} />
+                <Input type="password" placeholder="Mot de passe" name="password" required value={password} onChange={handleChange} />
                 <Button type="submit" text="VALIDER" path="" />
             </form>
         </main>
