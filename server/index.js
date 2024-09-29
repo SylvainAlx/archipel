9b94375bc1d61e47241c74d779cdd185034bdf14
Sylvain Alexandre <sylvainalx.dev@gmail.com>
2024-09-29 14:32:55 +0200

    server - correction index.js

--- server/index.js
+++ server/index.js
@@ -1,3 +1,7 @@
+import dotenv from "dotenv";
+dotenv.config(); // Charger les variables d'environnement dès le début
+
+import mongoose from "mongoose";
 import express from "express";
 import cors from "cors";
 import { home } from "./views/serverHome.js";
@@ -10,36 +14,45 @@ import userRouter from "./routers/userRouter.js";
 import { verifyJwt } from "./middlewares/authMiddleware.js";
 import { deleteUploadedFile } from "./controllers/files.js";
 import relationRouter from "./routers/relationRouter.js";
-import { runMongoDB } from "./utils/db.js";
 
 // config serveur
 const app = express();
 const PORT = 3000;
-app.use(cors());
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-try {
-  runMongoDB().catch(console.dir);
-} catch (error) {
-  console.log(error);
+if (!process.env.MONGO_DB_URI) {
+  console.error("MONGO_DB_URI n'est pas défini dans le fichier .env");
+  process.exit(1); // Quitte l'application avec un code d'erreur
 }
 
-try {
-  app.listen(PORT, () => {
-    console.log(`server running at PORT : ${PORT}`);
-    app.use("/user", userRouter);
-    app.use("/auth", authRouter);
-    app.use("/nation", nationRouter);
-    app.use("/com", comRouter);
-    app.use("/place", placeRouter);
-    app.use("/param", paramRouter);
-    app.use("/relation", relationRouter);
-    app.delete("/file/delete/:id", [verifyJwt], deleteUploadedFile);
-    app.use("/", home);
-  });
-} catch (error) {
-  console.log(error);
-}
+const connectToDatabase = async () => {
+  try {
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(process.env.MONGO_DB_URI);
+    console.log("connexion à la base de données");
+  } catch (error) {
+    console.log("Erreur lors de la connexion à la base de données :", error);
+  }
+};
+
+connectToDatabase();
+
+// Définition des routes
+app.use("/user", userRouter);
+app.use("/auth", authRouter);
+app.use("/nation", nationRouter);
+app.use("/com", comRouter);
+app.use("/place", placeRouter);
+app.use("/param", paramRouter);
+app.use("/relation", relationRouter);
+app.delete("/file/delete/:id", [verifyJwt], deleteUploadedFile);
+app.use("/", home);
+
+// Démarrage du serveur
+app.listen(PORT, () => {
+  console.log(`server running at PORT : ${PORT}`);
+});
